042383d6f66b58af9dac5f3cf17d885e353714ba
wagnerfelten <69311486+wagnerfelten@users.noreply.github.com>
2022-09-29 11:57:41 -0300

    feito operacao de conctenar os numeros

--- js/controllers/calcController.js
+++ js/controllers/calcController.js
@@ -1,6 +1,7 @@
 class calcController{
     constructor(){
 
+        this._operation = [];
         this._locale = "pt-br";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -10,6 +11,7 @@ class calcController{
         this._currentDate;
         this.init();
         this.initButtonsEvents();
+        
     }
 
     init(){
@@ -31,8 +33,122 @@ class calcController{
     addEventListenerAll(element, events, fn){
 
             events.split(' ').forEach(event =>{
-                element.addEventListener();
-            })
+                element.addEventListener(event, fn, false);//com o false sera gerado apenas um evento de click e nao dois, pois sendo o botao e string atuando dividualmente 
+            });
+    }
+
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    getLastOperation(){
+
+        return this._operation[this._operation.length-1]; //vai pegar todos os arreys e tirar o ultimo
+    
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length - 1] = value;
+    
+    }
+
+    isOperation(value){
+    
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+    
+    }
+
+    addOperation(value){
+
+        if(isNaN(this.getLastOperation())){
+
+            if(this.isOperation(value)){
+
+                this.setLastOperation(value);
+            
+            }else if(isNaN(value)){
+                console.log(value)
+
+            }else{
+                this._operation.push(value);
+            }
+
+        }else{
+            let newValue = this.getLastOperation().toString() + value.toString(); //vai tranformar o numero em String e conctenar os numeros em forma de string
+            
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        
+
+        console.log(this._operation);
+    }
+
+    setError(){
+        this.displayCalc = "ERROR"
+    }
+
+    execBtn(value){
+        switch(value){
+            case 'ac':
+                    this.clearAll();
+                break;
+
+            case 'ce':
+                    this.clearEntry();
+                break;
+
+            case 'soma':
+                this.addOperation('+');
+                break;
+                   
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            
+            case 'divisao':
+                this.addOperation('/');
+                break;
+
+            case 'porcento':
+                this.addOperation('%');
+                break;
+
+            case 'igual':
+
+                break;
+            
+            case 'ponto':
+                this.addOperation('.');
+                break;
+            
+            case '0':
+            case '1':            
+            case '2':
+            case '3':                    
+            case '4':
+            case '5':                            
+            case '6':
+            case '7':                                    
+            case '8':
+            case '9':
+                    this.addOperation(parseInt(value));                                            
+                break;
+            
+            
+            default:
+                    this.setError();
+                break;        
+        }
     }
 
     initButtonsEvents(){
@@ -41,11 +157,17 @@ class calcController{
 
             buttons.forEach((btn, index) =>{
                 
-                this.addEventListenerAll("click drag", e => {
+                this.addEventListenerAll(btn, "click drag", e => {
                 
-                    console.log(btn.className.baseVal.replace("btn-" , ""));
+                    let textBtn = btn.className.baseVal.replace("btn-" , "");
+
+                    this.execBtn(textBtn);
                 });
-            });
+
+                this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
+                    btn.style.cursor = "pointer";
+                });
+            })
 
     }
 
